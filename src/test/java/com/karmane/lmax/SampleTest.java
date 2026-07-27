0a8496b58de039305aba730e434f5ecb592e99fd
Fouad Karmane <8707605+FouadKarmane@users.noreply.github.com>
2026-07-27 23:08:53 +0100

    tiny clean-up

--- src/test/java/com/karmane/lmax/SampleTest.java
+++ src/test/java/com/karmane/lmax/SampleTest.java
@@ -1,6 +1,5 @@
 package com.karmane.lmax;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
